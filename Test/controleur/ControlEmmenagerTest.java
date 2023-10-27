a3ca1e2adb092a996e8483073dc50c6fdb990145
Antoine <antoine.vallat31@gmail.com>
2023-10-27 11:29:18 +0200

    Teste 1

---
+++ Test/controleur/ControlEmmenagerTest.java
@@ -0,0 +1,30 @@
+package controleur;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import personnages.*;
+import villagegaulois.Village;
+
+
+class ControlEmmenagerTest {
+	private Village village;
+	private Chef chef ;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		village = new Village ("Le village des iréductibles", 10 ,5);
+		chef = new Chef("Abracourcix" , 10  , village);
+		}
+	@Test
+	void test() {
+		ControlEmmenager crtlEmmanager = new ControlEmmenager(village);
+		crtlEmmanager.ajouterGaulois("Bonemine", 3);
+		crtlEmmanager.ajouterDruide("Panoramix", 6,4,8);
+		assertFalse(crtlEmmanager.isHabitant("Bonemine"));
+		assertFalse(crtlEmmanager.isHabitant("Panoramix"));
+	}
+
+}
